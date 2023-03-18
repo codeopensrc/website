4252c9dddbb44a54a6535a3270ff80840a82d1b5
kc <kcjones.2002@gmail.com>
2023-03-18 06:47:31 +0000

    pm2 tz and upd dep chart
    https://gitlab.codeopensrc.com/kc/website/-/merge_requests/26

--- server/pm2.config.js
+++ server/pm2.config.js
@@ -1,14 +1,21 @@
 "use strict";
 
+const PM2_TZ = process.env.PM2_TZ || "UTC"
+
 module.exports = {
     apps: [{
         "name": "App",
         "cwd": "./",
         "watch": ["server"],
+        "watch_options": {
+            "usePolling": true,
+            "interval": 300,
+            "binaryInterval": 300
+        },
         "script": "./server/server.js",
         "out_file": "./logs/server-out.log",
         "error_file": "./logs/server-err.log",
-        "log_date_format": "MM-D-YY, h:mm:ss a",
+        "log_date_format": "MM-D-YY, h:mm:ss a Z",
         "min_uptime": 10000,
         "max_restarts": 3,
         // We'll still use output until we configure a way to create files and send
@@ -17,6 +24,9 @@ module.exports = {
         "exec_mode": "cluster",
         "instances": 1,
         "kill_timeout" : 8000,
-        "wait_ready": true
+        "wait_ready": true,
+        "env": {
+            "TZ": `${PM2_TZ}`
+        }
   }]
 }
